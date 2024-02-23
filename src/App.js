1d1f92c345b09ae3bc6fdf3ada566942f1569e62
saijami <saijami@unosimple.com>
2024-02-23 12:56:46 +0530

    ui Changes

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo-rev" alt="logo" />
         <p>{/* Edit <code>src/App.js</code> and save to reload. */}</p>
         {/* <ReduceMethod /> */}
         {/* <JavaArray /> */}
