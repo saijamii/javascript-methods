bc9d7553ab29998e6f8779e243aefe79d3c402ba
saijamii <saijami38@gmail.com>
2024-02-23 08:01:14 +0530

    changes

---
+++ src/C1/Call.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+function Call() {
+  let User = {
+    name: "A",
+    age: 20,
+    printDetails: function () {
+      console.log(this.name);
+    },
+  };
+
+  User.printDetails();
+
+  let User2 = {
+    name: "B",
+    age: 30,
+    printDetails: function () {
+      console.log(this.name);
+    },
+  };
+
+  User2.printDetails();
+
+  let User3 = {
+    name: "C",
+    age: 40,
+    printDetails: function () {
+      console.log(this.name);
+    },
+  };
+
+  User3.printDetails();
+
+  return <div>call</div>;
+}
+
+export default Call;
