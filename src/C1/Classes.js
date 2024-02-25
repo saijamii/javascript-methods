6d38436fbe8265e37a8d06e0c6bbe2491324a685
saijamii <saijami38@gmail.com>
2024-02-25 20:09:35 +0530

    Classes chanegs

--- src/C1/Classes.js
+++ src/C1/Classes.js
@@ -37,6 +37,34 @@ function Classes() {
   myTri2.getArea();
   myTri2.getSides();
 
+  class Triangle {
+    constructor(a, b, c) {
+      this.sideA = a;
+      this.sideB = b;
+      this.sideC = c;
+    }
+
+    getArea() {
+      console.log((this.sideA + this.sideB) / 2, "getArea");
+    }
+
+    getSides() {
+      console.log({ sideA: this.sideA, sideB: this.sideB, sideC: this.sideC });
+    }
+  }
+
+  let myTriangle1 = new Triangle(5, 6, 7);
+  myTriangle1.getArea();
+  myTriangle1.getSides();
+
+  let myTriangle2 = new Triangle(10, 11, 12);
+  myTriangle2.getArea();
+  myTriangle2.getSides();
+
+  let myTriangle3 = new Triangle(20, 30, 40);
+  myTriangle3.getArea();
+  myTriangle3.getSides();
+
   return <div>Classes</div>;
 }
 
