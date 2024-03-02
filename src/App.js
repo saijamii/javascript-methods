e686cd59c5d5ba4d0aa5f1177b55adc1721d1b55
saijamii <saijami38@gmail.com>
2024-03-02 19:21:38 +0530

    Promise Imported

--- src/App.js
+++ src/App.js
@@ -6,20 +6,21 @@ import Response from "./Response";
 import Prac from "./Prac";
 import Call from "./C1/Call";
 import Classes from "./C1/Classes";
+import { Promisese1, Promisese2 } from "./C1/Promise";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {/* <img src={logo} className="App-logo-rev" alt="logo" /> */}
-        <p>{/* Edit <code>src/App.js</code> and save to reload. */}</p>
         <ReduceMethod />
         <JavaArray />
         <Response />
         <Prac />
         <Call />
         <Classes />
+        <Promisese1 />
+        <Promisese2 />
       </header>
     </div>
   );
