2848f1f4091e254457506e45e1607a2268284a65
saijamii <saijami38@gmail.com>
2024-03-05 07:19:50 +0530

    MyUseMemo2 changes

--- src/R1/RUseMemo.js
+++ src/R1/RUseMemo.js
@@ -1,10 +1,28 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
 
 export const MyUseMemo1 = ({ a, b }) => {
   const sum = useMemo(() => {
-    console.log("Calculating sum");
     return a + b;
   }, [a, b]);
 
   return <div>Sum: {sum}</div>;
+};
+
+export const MyUseMemo2 = () => {
+  const [count, setCount] = useState(0);
+
+  const result = useMemo(() => {
+    return expensiveFunction(count);
+  }, [count]);
+
+  return (
+    <div>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <div>Result: {result}</div>
+    </div>
+  );
+};
+
+const expensiveFunction = (value) => {
+  return value * 2;
 };
