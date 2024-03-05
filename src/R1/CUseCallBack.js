0e452c1fcb50809f54c8f25670b3e5f3e77583d3
saijami <saijami@unosimple.com>
2024-03-05 14:51:30 +0530

    changes

---
+++ src/R1/CUseCallBack.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function CUseCallBack() {
+  return <div>CUseCallBack</div>;
+}
---
+++ src/R1/PUseCallBack.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function PUseCallBack() {
+  return <div>PUseCallBack</div>;
+}
