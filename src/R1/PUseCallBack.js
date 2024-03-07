e5db777bf209341318bf501f42fe8a6e3346542c
saijamii <saijami38@gmail.com>
2024-03-07 08:21:49 +0530

    props passing

--- src/R1/PUseCallBack.js
+++ src/R1/PUseCallBack.js
@@ -3,14 +3,19 @@ import CUseCallBack from "./CUseCallBack";
 
 export function PUseCallBack() {
   const [countOne, setCountOne] = useState(0);
+  const [countTwo, setCountTwo] = useState(0);
 
   const IncrementOne = () => {
     setCountOne(countOne + 1);
   };
 
+  const myFunction = () => {
+    console.log("My Function");
+  };
+
   return (
     <>
-      <CUseCallBack />
+      <CUseCallBack countTwo={countTwo} myFunction={myFunction} />
       <button
         onClick={() => {
           IncrementOne();
