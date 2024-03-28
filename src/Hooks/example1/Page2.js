4d1ecc5ebf45e0c894917462652a66123a5ab4fa
saijami <saijami@unosimple.com>
2024-03-28 14:39:23 +0530

    Buttons counter added

--- src/Hooks/example1/Page2.js
+++ src/Hooks/example1/Page2.js
@@ -1,5 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Page2() {
-  return <div>Page2</div>;
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Count -- {count}
+      </button>
+    </div>
+  );
 }
