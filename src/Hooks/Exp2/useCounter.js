80523e01fc3611898b6e4c6f1ecd07361c249052
saijamii <saijami38@gmail.com>
2024-03-29 08:32:18 +0530

    useCounter initialValue

--- src/Hooks/Exp2/useCounter.js
+++ src/Hooks/Exp2/useCounter.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 export function useCounter(initialValue = 0) {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(initialValue);
 
   const increment = () => {
     setCount(count + 1);
