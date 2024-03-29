3560f770369758b8f63749f923b3724819418743
saijamii <saijami38@gmail.com>
2024-03-29 08:03:46 +0530

    useDocumentTitle import in Comp

--- src/Hooks/Exp1/Comp2.js
+++ src/Hooks/Exp1/Comp2.js
@@ -1,12 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
+import { useDocumentTitle } from "./useDocumentTitle";
 
 export default function Comp2() {
-    
   const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    document.title = count;
-  }, [count]);
+  useDocumentTitle(count);
 
   return <button onClick={() => setCount(count + 1)}>count - {count}</button>;
 }
