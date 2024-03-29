7b7a01f86fb9ab69fcde690bde829d6f87b5cc03
saijamii <saijami38@gmail.com>
2024-03-29 07:59:27 +0530

    document.title init

--- src/Hooks/Exp1/Comp1.js
+++ src/Hooks/Exp1/Comp1.js
@@ -1,6 +1,12 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Comp1() {
+    
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = count;
+  }, [count]);
+
   return <button onClick={() => setCount(count + 1)}>count - {count}</button>;
 }
